26f01c4d56d399c1140afdfccaa670484b7b0e61
Arthur Oliveira <arthurolvmorais@gmail.com>
2020-07-25 16:18:24 -0300

    feat(notification): Cria componente de notificações

---
+++ src/components/AppNotification.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="notification is-primary" v-if="state.visible">
+    <button class="delete" @click="close()"></button>
+    <div v-html="state.message" class="has-text-dark"></div>
+  </div>
+</template>
+
+<script>
+import Store from '@/store';
+
+export default {
+  data: () => ({ visible: false }),
+
+  computed: {
+    state() {
+      return Store.state.notification;
+    },
+  },
+
+  methods: {
+    close() {
+      Store.mutations.MutateNotification({ visible: false, message: '' });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.notification {
+  left: 100px;
+  bottom: 50px;
+  max-width: 50vh;
+  position: absolute;
+}
+
+@media (max-width: 768px) {
+  .notification {
+    left: 0;
+    margin: 0 20px;
+    width: calc(100% - 40px);
+  }
+}
+</style>
