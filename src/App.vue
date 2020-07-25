ec41e8f58d44b3a3b008b00e8716750b5727d4b8
Arthur Oliveira <arthurolvmorais@gmail.com>
2020-07-25 12:13:19 -0300

    feat(navbar): Cria navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <div id="app">
+    <app-navbar />
     <router-view/>
   </div>
 </template>
+
+<script>
+import AppNavbar from './components/AppNavbar.vue';
+
+export default {
+  name: 'App',
+
+  components: { AppNavbar },
+};
+</script>
 
 <style lang="scss"></style>
---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="navbar is-light" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://bulma.io">
+      <img src="@/assets/img/logo.svg" width="30" height="30" alt="logo">
+      <span class=" ml-2">Lojinea de bolos</span>
+    </a>
+  </div>
+
+  <div class="navbar-menu">
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-light">
+            <span class="icon">
+              <img src="@/assets/img/cesta.svg" alt="cesta">
+            </span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+  name: 'AppNavbar',
+};
+</script>
